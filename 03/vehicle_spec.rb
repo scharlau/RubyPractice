1ee6e9386dd7a7cc8f3a225503eec32ec85fbfff
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2011-02-22 10:10:48 +0000

    fixed vehicle test for hash

--- 03/vehicle_spec.rb
+++ 03/vehicle_spec.rb
@@ -7,26 +7,26 @@ describe "the features and style of the vehicle" do
   end
   
   it "should be possible to choose how it moves" do
-    Vehicle.new.travel_options.should == ["fly","drive","float", "hover"]
+    Vehicle.new("Ruby").travel_options.should == ["fly","drive","float", "hover"]
   end
   
   it "should be possible to specifiy how it moves" do
-     @plane = Vehicle.new
+     @plane = Vehicle.new("phil")
      @plane.travel_choice(1)
     @plane.travel_mode.should == "fly"
   end
   
   it "should be possible to make a suitable sound when moving" do
-    @plane = Vehicle.new
+    @plane = Vehicle.new("george")
     @plane.travel_choice(1)
     @plane.sound.should == "whoosh"
   end
   
-  it "should be possible to store a number of vehicles and their types" do
+  it "should be possible to store a number of vehicles and their travel_modes" do
     @plane = Vehicle.new("sally")
     @plane.travel_choice(1)
-    @car = Vehicle.new
+    @car = Vehicle.new("george")
     @car.travel_choice(2)
-    @car.list.should include("george", "drive", "fly","sally")
+    @car.list.should include(@car.list["george"], @car.list["sally"])
   end
 end
